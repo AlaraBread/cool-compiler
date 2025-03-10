f24e63a87f1953d6da2fe61cfcd9f06e08b89c43
Abigail <abigail@dogbuilt.net>
2025-03-10 11:30:32 -0400

    Write a hello world in Haskell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A COOL compiler";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      # User-friendly version number
+      version = builtins.substring 0 8 self.lastModifiedDate;
+
+      # System types to support
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system: {
+        default = let pkgs = nixpkgsFor.${system};
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [ ghc haskell-language-server ormolu ];
+        };
+      });
+    };
+}

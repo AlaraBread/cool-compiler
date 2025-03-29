2805b93aeee87e67a3454fe82ade0e6788e6ea6a
Abigail <abigail@dogbuilt.net>
2025-03-28 20:02:56 -0400

    Add glibc.static to nix shell

--- flake.nix
+++ flake.nix
@@ -19,7 +19,12 @@
       devShells = forAllSystems (system: {
         default = let pkgs = nixpkgsFor.${system};
         in pkgs.mkShell {
-          buildInputs = with pkgs; [ haskell.compiler.ghc810 haskell-language-server ormolu ];
+          buildInputs = with pkgs; [
+            haskell.compiler.ghc810
+            haskell-language-server
+            ormolu
+            glibc.static
+          ];
         };
       });
     };
